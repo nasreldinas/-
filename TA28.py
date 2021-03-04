771b99c7f8f46aaebe57ed931ca475ab5413e73c
nasreldinas <79415927+nasreldinas@users.noreply.github.com>
2021-03-04 08:21:05 -0800

    Update and rename Ta28 to TA28.py

--- TA28.py
+++ TA28.py
@@ -1,3 +1,14 @@
+#КОД ПРИМЕРА «Ветвления и оператор выбора»
+#номер версии: 1.0
+#имя файла: TA28.py
+#автор и его учебная группа: A. nasreldin, ЭУ-135
+#дата создания: 27.02.2021
+#дата последней модификации: 04.03.2021
+#описание: Ветвления и оператор выбора
+#версия Python:3.9.2
+#Составить алгоритм и программу для реализации логических операций «И» и «ИЛИ» для двух переменных.
+
+
 import random
 
 time = ("morning", "noon", "evening",  "night")
@@ -18,3 +29,12 @@ elif (TIME == "noon" and SEASON == "winter") or (TIME == "morning" and SEASON ==
 
 elif (TIME == "night" and SEASON == "summer") or (TIME == "evening" and SEASON == "summer") or (TIME == "noon" and SEASON == "summer") or (TIME == "morning" and SEASON == "summer"):
     print("It's warm outside!!!")
+    
+    
+    #результат
+Time: evening
+Season: winter
+
+WEATHER FORECAST:
+
+It's very cold outside!!!
