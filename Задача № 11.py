58d2fd032669b16a055a8363503e74f21ffeb2c1
nasreldinas <79415927+nasreldinas@users.noreply.github.com>
2021-06-18 06:17:55 +0300

    Create Задача № 11.py

---
+++ Задача № 11.py
@@ -0,0 +1,75 @@
+#КОД ПРИМЕРА «Работа с двумерными массивами (матрицами)»
+#номер версии: 1.0
+#имя файла: Задача № 11.py
+#автор и его учебная группа: A. nasreldin, ЭУ-135
+#дата создания: 18.06.2021
+#дата последней модификации: 18.06.2021
+#описание: Ветвления и оператор выбора
+#версия Python:3.9.2
+
+import random
+
+N = 5  # строк
+M = 7  # столбцов
+
+K = 2
+
+
+def get_row(column):
+    col = []
+    for i in range(0, column):
+        col.append(random.randint(0, 9))
+
+    return col
+
+
+def get_matrix(row, column):
+    matrix = []
+    for i in range(0, row):
+        matrix.append(get_row(column))
+
+    return matrix
+
+
+def print_matrix(matrix):
+    i = 0
+    while i < len(matrix):
+        j = 0
+        row = matrix[i]
+        while j < len(row):
+            column = row[j]
+            print(column, end=' ')
+            j += 1
+
+        print()
+        i += 1
+
+
+def get_column(matrix, index):
+    column = []
+    i = 0
+    while i < len(matrix):
+        column.append(matrix[i][index])
+        i += 1
+
+    return column
+
+
+A = get_matrix(N, M)
+print("Исходная матрица:")
+print_matrix(A)
+
+k_column = get_column(A, K - 1)
+
+i = 0
+while i < len(A):
+    j = 0
+
+    while j < len(A[i]):
+        A[i][j] *= k_column[i]
+        j += 1
+
+    i += 1
+
+print("Моифицированная матрица:")
+print_matrix(A)
